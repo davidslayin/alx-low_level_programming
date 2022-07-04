ed168c7cabb8a5b74fddc6456c80632097e1c804
davidslayin <dave191994@gmail.com>
2022-07-04 14:23:48 -0700

    showing the size of different types

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,3 @@
-#include<stdio.h>
-
-/**
- * main- entry point
- *
- * printf displays the text
- * Return: 0 after the text
- */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-}
+[master 52e6fa5] printing a text using printf
+ 3 files changed, 15 insertions(+), 12 deletions(-)
+ create mode 100644 0x00-hello_world/5-printf.c
