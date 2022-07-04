663e70fdfe3010394d6915a59475442efdf75b13
davidslayin <dave191994@gmail.com>
2022-07-04 13:50:38 -0700

    printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
+ * main- entry point
  *
- * puts - function displaying a text with a new line
- *
- * Return:  0 after displaying the text (success)
+ * Returns: 0 after displaying text
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
