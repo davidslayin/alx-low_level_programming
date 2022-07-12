7ec38bf8a2a59fd8406dfdb9eda3e27ff084ddd6
your name <dave191994@gmail.com>
2022-07-12 14:45:16 -0700

    fixing the print rev task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,28 +2,24 @@
 #include <sting.h>
 
 /**
- * rev_string - printing the string reversed
+ * print_rev - print the string in reverse
  * @s: string
  * Return: void is returned
  */
 void print_rev(char *s)
 {
-	int i, max, half;
-	char first, last;
+	int i = 0;
 
-	i = 0;
 	while (s[i] != '\0')
 	{
 	i++;
 	}
-	max = i - 1;
-	half = max / 2;
-	while (half >= 0)
+	i--;
+	while (i >= 0)
 	{
-	first = s[max - half];
-	last = s[half];
-	s[half] = first;
-	s[max - half] = last;
-	half--;
+	_putchar(s[i]);
+	i--;
 	}
+	_putchar('\n');
+
 }
