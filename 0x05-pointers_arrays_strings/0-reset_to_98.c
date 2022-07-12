97c7917fc40793fcc0ed233a3b298153ddca99bf
your name <dave191994@gmail.com>
2022-07-12 10:44:58 -0700

    fixing documentation

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rest_to_98 - a function to update the reference of a pointer
+ * reset_to_98 - a function to update the reference of a pointer
  * @n: pointer to n which will be updated
  */
 void reset_to_98(int *n)
