4a8cf291cf5d07c42110f6b04e37d78e804e3fce
your name <dave191994@gmail.com>
2022-07-12 15:04:53 -0700

    a function that prints everyother cahracter of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - printing alternating chars of string
+ * @str: string parameter
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+	if (i % 2 == 0)
+		_putchar(str[i]);
+	i++;
+	}
+	_putchar('\n');
+}
