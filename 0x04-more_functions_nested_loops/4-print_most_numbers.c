360c10e50ff96f35117a7551b3bc2b8963dc2d19
your name <dave191994@gmail.com>
2022-07-14 08:24:31 -0700

    printing numbers strating from 0 to 9 except for 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers 0 to 9
+ * except for 2 and 4
+ * Return: none
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+	if (n != 2 && n != 4)
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
