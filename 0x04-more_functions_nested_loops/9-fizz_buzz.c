1abe32b976a1b9ad1b1e722f60fcac2527723855
your name <dave191994@gmail.com>
2022-07-14 10:45:50 -0700

    fizz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point for fizz test
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0)
+	{
+	printf("Fizz\n");
+	}
+	else if (i % 5 == 0)
+	{
+	printf("FuzzBuzz\n");
+	}
+	else
+	{
+	printf("%d\n", i);
+	}
+	}
+	return (0);
+}
