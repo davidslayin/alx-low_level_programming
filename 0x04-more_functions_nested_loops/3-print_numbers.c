0ecffdd77305ec82679cd9a7f2ab77f78b5b95bc
your name <dave191994@gmail.com>
2022-07-14 08:01:38 -0700

    printing a numbers between 0 and 9 followed by new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers 0 to 9
+ *
+ * Return: none
+ */
+void print_numbers(void)
+{
+	int n;
+for (n = 0; n <= 9; n++)
+	_putchar(n + '0');
+	_putchar('\n');
+}
