e7b9ed677b407db50472d01b04d13651eada201f
your name <dave191994@gmail.com>
2022-07-14 16:56:45 -0700

    ecodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - the function
+ * @str: parameter
+ * Return: return str
+ */
+char *leet(char *str)
+{
+char small[] = {'a', 'e', 'o', 't', 'l'};
+char capital[] = {'A', 'E', 'O', 'T', 'L'};
+char leet[] = {'4', '3', '0', '7', '1'}
+int i, j;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; j < 5; j++)
+{
+if (str[i] == small[j] || str[i] == capital[j])
+{
+str[i] = leet[j];
+}
+}
+}
+return (str);
+}
