cd3e2d685b56122b34135242b99c8b723ce3720e
your name <dave191994@gmail.com>
2022-07-14 10:54:04 -0700

    fixing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,9 +12,13 @@ int main(void)
 	{
 	if (i % 3 == 0)
 	{
-	printf("Fizz \n");
+	printf("Fizz\n");
 	}
 	else if (i % 5 == 0)
+	{
+	printf("Buzz\n");
+	}
+	else if(i % 3 == 0 && i % 5 == 0)
 	{
 	printf("FuzzBuzz\n");
 	}
