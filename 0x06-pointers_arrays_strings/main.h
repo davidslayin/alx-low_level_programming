399fd08f9cecffb24b382ddd564647ee28bebb99
your name <dave191994@gmail.com>
2022-07-14 13:58:18 -0700

    fixing the code

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,4 @@
 #ifndef _main_H_
 #define _main_H_
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
 #endif
