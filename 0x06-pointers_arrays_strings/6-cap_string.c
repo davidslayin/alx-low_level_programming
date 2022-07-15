925a39d615db7a0c5ed9bda6e49cc673b88e5497
your name <dave191994@gmail.com>
2022-07-14 17:22:02 -0700

    fixing the cap string task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - capitalize the letters of a string
+ * @str: parametr
+ * Return: return str
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i == 0)
+{
+if ((str[i] >= 'a' && str[i] <= 'z'))
+str[i] = str[i] - 32;
+continue;
+}
+if (str[i] == '\n' || str[i] == '!' || str[i] == ',' || str[i] == ';' ||
+		str[i] == '.' || str[i] == '?' || str[i] == '"' ||
+		str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+{
+++i;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+continue;
+}
+}
+}
+return (str);
+}
