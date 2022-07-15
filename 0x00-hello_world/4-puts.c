38bc5988eb26ac2a2817adb5daf514ed630fa25c
davidslayin <dave191994@gmail.com>
2022-07-15 18:17:47 +0300

    Delete 0x00-hello_world directory

--- 0x00-hello_world/4-puts.c
+++
@@ -1,2 +0,0 @@
-[master 03c2081] pring with puts
- 1 file changed, 2 insertions(+), 1 deletion(-)
--- 0x00-hello_world/6-size.c
+++
@@ -1,4 +0,0 @@
-[master ed168c7] showing the size of different types
- 3 files changed, 20 insertions(+), 13 deletions(-)
- create mode 100644 0x00-hello_world/6-size.c
- create mode 100755 0x00-hello_world/size32
