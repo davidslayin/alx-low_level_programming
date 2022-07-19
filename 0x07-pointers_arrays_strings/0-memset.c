e0cf2951d0a02c3f45fcd863d519322ef586a7fa
your name <dave191994@gmail.com>
2022-07-19 08:21:36 -0700

    0-memset.c task done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memset - a function that fills memory with a constant byte
+ *@s: first parameter
+ *@b: second parameter
+ *@n: third parameter
+ *Return: returns s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+*s++ = b;
+}
+return s;
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is a readme for  even more pointers project
