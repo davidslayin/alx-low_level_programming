05b9cdbaa25c34c65d6f2b2bed85d8dae6559639
your name <dave191994@gmail.com>
2022-07-20 10:15:49 -0700

    fix the code for prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,7 @@ int check_prime(int, int);
  */
 int is_prime_number(int n)
 {
-return (check_prime(n, 1));
+return (check_prime(in, 1));
 }
 /**
  * check_prime - checking whether its prime or not
@@ -20,7 +20,7 @@ int check_prime(int n, int i)
 {
 if (n <= 1)
 	return (0);
-if (n % 1 == 0 && i > 1)
+if (n % i == 0 && i > 1)
 	return (0);
 if ((n / i) < i)
 	return (1);
