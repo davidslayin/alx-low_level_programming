eb4397b5b237cc0cfe291483fed9e70df11aa384
your name <dave191994@gmail.com>
2022-07-20 09:48:06 -0700

    sqrt task done

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - square root using recursion
+ * @n: number parameter
+ * Return: returns the square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - recursive square root
+ * @n: number param
+ * @i: iterator param
+ * Return: a number
+ */
+int _sqrt(int n, int i)
+{
+	int square = i * i;
+
+if (square > n)
+	return (-1);
+if (square == n)
+	return (i);
+return (_sqrt(n, i + 1));
+}
