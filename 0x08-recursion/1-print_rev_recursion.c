cb7d93668c37571e8994ee64365fd0861d17fd9d
your name <dave191994@gmail.com>
2022-07-20 08:42:28 -0700

    printing a string in reverse task done

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - printing a reversed string using recursion
+ * @s: string parameter
+ */
+void _print_rev_recursion(char *s)
+{
+if (s == '\0')
+	return;
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
+}
