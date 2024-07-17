8b1c084f58cd9e9f26634a8fdb26562f87749c1b
davidslayin <dave191994@gmail.com>
2024-07-17 08:33:39 -0700

    linear search task done

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,24 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/*
+ * File: search_algos.h
+ * Desc: This header file contains all the prototypes
+ * required for search algorithm project
+ *
+ * Author: Dawit Alemayehu
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
